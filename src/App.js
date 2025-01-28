ba4dcc745b725bccd966c6c39bb40f929580947f
MohamedHnef <mohamed.hnef1@gmail.com>
2025-01-28 14:43:21 +0200

    Initial Create React App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// App.js
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>ShenCarCar</h1>
+      <p>Welcome to the React Project!</p>
+    </div>
+  );
+}
+
+export default App;
